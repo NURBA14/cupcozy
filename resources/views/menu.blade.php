7a03befe727a13f24bb92ce137ff2d1d4b763147
NURBA14 <muradnurbolat85@gmail.com>
2026-04-08 04:43:10 +0500

    Add menu

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -18,12 +18,12 @@
         <div class="container">
             <div class="section-title">
                 <h4 class="text-primary text-uppercase" style="letter-spacing: 5px;">Меню & Цены</h4>
-                <h1 class="display-4">Цены</h1>
+                <h1 class="display-4">Меню</h1>
             </div>
             <div class="row">
+                {{-- Горячий кофе --}}
                 <div class="col-lg-6">
                     <h1 class="mb-5">Горячий кофе</h1>
-                    {{-- Эспрессо --}}
                     <div class="row align-items-center mb-5">
                         <div class="col-4 col-sm-3">
                             <img class="w-100 rounded-circle mb-3 mb-sm-0" src="{{ asset("img/menu-1.jpg") }}" alt="">
@@ -35,7 +35,6 @@
                             </p>
                         </div>
                     </div>
-                    {{-- Американо --}}
                     <div class="row align-items-center mb-5">
                         <div class="col-4 col-sm-3">
                             <img class="w-100 rounded-circle mb-3 mb-sm-0" src="{{ asset("img/menu-4.jfif") }}" alt="">
@@ -47,7 +46,6 @@
                             </p>
                         </div>
                     </div>
-                    {{-- Капучино --}}
                     <div class="row align-items-center mb-5">
                         <div class="col-4 col-sm-3">
                             <img class="w-100 rounded-circle mb-3 mb-sm-0" src="{{ asset("img/menu-2.jpg") }}" alt="">
@@ -59,7 +57,6 @@
                             </p>
                         </div>
                     </div>
-                    {{-- Латте --}}
                     <div class="row align-items-center mb-5">
                         <div class="col-4 col-sm-3">
                             <img class="w-100 rounded-circle mb-3 mb-sm-0" src="{{ asset("img/menu-3.jpg") }}" alt="">
@@ -72,9 +69,9 @@
                         </div>
                     </div>
                 </div>
+                {{-- Холодный кофе --}}
                 <div class="col-lg-6">
                     <h1 class="mb-5">Холодный кофе</h1>
-                    {{-- Айс Американо --}}
                     <div class="row align-items-center mb-5">
                         <div class="col-4 col-sm-3">
                             <img class="w-100 rounded-circle mb-3 mb-sm-0" src="{{ asset("img/menu-5.jpg") }}" alt="">
@@ -86,7 +83,6 @@
                             </p>
                         </div>
                     </div>
-                    {{-- Айс Латте --}}
                     <div class="row align-items-center mb-5">
                         <div class="col-4 col-sm-3">
                             <img class="w-100 rounded-circle mb-3 mb-sm-0" src="{{ asset("img/menu-6.png") }}" alt="">
@@ -98,7 +94,6 @@
                             </p>
                         </div>
                     </div>
-                    {{-- Фраппе --}}
                     <div class="row align-items-center mb-5">
                         <div class="col-4 col-sm-3">
                             <img class="w-100 rounded-circle mb-3 mb-sm-0" src="{{ asset("img/menu-7.jpeg") }}" alt="">
@@ -110,7 +105,6 @@
                             </p>
                         </div>
                     </div>
-                    {{-- Мокко Айс --}}
                     <div class="row align-items-center mb-5">
                         <div class="col-4 col-sm-3">
                             <img class="w-100 rounded-circle mb-3 mb-sm-0" src="{{ asset("img/menu-8.webp") }}" alt="">
@@ -123,7 +117,103 @@
                         </div>
                     </div>
                 </div>
+                {{-- Фастфуд --}}
+                <div class="col-lg-6">
+                    <h1 class="mb-5">Фастфуд</h1>
+                    <div class="row align-items-center mb-5">
+                        <div class="col-4 col-sm-3">
+                            <img class="w-100 rounded-circle mb-3 mb-sm-0" src="{{ asset("img/menu-9.jfif") }}" alt="">
+                        </div>
+                        <div class="col-8 col-sm-9">
+                            <h4>Сэндвич с курицей — 1890₸</h4>
+                            <p class="m-0">
+                                Сэндвич с сочной курицей, свежими овощами и соусом в мягком хлебе.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row align-items-center mb-5">
+                        <div class="col-4 col-sm-3">
+                            <img class="w-100 rounded-circle mb-3 mb-sm-0" src="{{ asset("img/menu-10.jpg") }}" alt="">
+                        </div>
+                        <div class="col-8 col-sm-9">
+                            <h4>Сэндвич с ветчиной и сыром — 1790₸</h4>
+                            <p class="m-0">
+                                Классический сэндвич с ветчиной, сыром, овощами и соусом.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row align-items-center mb-5">
+                        <div class="col-4 col-sm-3">
+                            <img class="w-100 rounded-circle mb-3 mb-sm-0" src="{{ asset("img/menu-11.webp") }}" alt="">
+                        </div>
+                        <div class="col-8 col-sm-9">
+                            <h4>Круассан с лососем — 1990₸</h4>
+                            <p class="m-0">
+                                Круассан с лососем и свежими овощами.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row align-items-center mb-5">
+                        <div class="col-4 col-sm-3">
+                            <img class="w-100 rounded-circle mb-3 mb-sm-0" src="{{ asset("img/menu-12.jpg") }}" alt="">
+                        </div>
+                        <div class="col-8 col-sm-9">
+                            <h4>Салат Цезарь — 1990₸</h4>
+                            <p class="m-0">
+                                Свежий салат с курицей, листьями салата, сухариками и соусом цезарь.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                {{-- Выпечка --}}
+                <div class="col-lg-6">
+                    <h1 class="mb-5">Выпечка</h1>
+                    <div class="row align-items-center mb-5">
+                        <div class="col-4 col-sm-3">
+                            <img class="w-100 rounded-circle mb-3 mb-sm-0" src="{{ asset("img/menu-16.jpeg") }}" alt="">
+                        </div>
+                        <div class="col-8 col-sm-9">
+                            <h4>Круассан — 990₸</h4>
+                            <p class="m-0">
+                                Слоёная выпечка с нежной текстурой и хрустящей корочкой.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row align-items-center mb-5">
+                        <div class="col-4 col-sm-3">
+                            <img class="w-100 rounded-circle mb-3 mb-sm-0" src="{{ asset("img/menu-13.webp") }}" alt="">
+                        </div>
+                        <div class="col-8 col-sm-9">
+                            <h4>Чизкейк — 1790₸</h4>
+                            <p class="m-0">
+                                Нежный десерт на основе сливочного сыра с песочной основой.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row align-items-center mb-5">
+                        <div class="col-4 col-sm-3">
+                            <img class="w-100 rounded-circle mb-3 mb-sm-0" src="{{ asset("img/menu-14.webp") }}" alt="">
+                        </div>
+                        <div class="col-8 col-sm-9">
+                            <h4>Шоколадный маффин — 790₸</h4>
+                            <p class="m-0">
+                                Мягкий кекс с насыщенным вкусом шоколада и влажной текстурой.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row align-items-center mb-5">
+                        <div class="col-4 col-sm-3">
+                            <img class="w-100 rounded-circle mb-3 mb-sm-0" src="{{ asset("img/menu-15.jpg") }}" alt="">
+                        </div>
+                        <div class="col-8 col-sm-9">
+                            <h4>Ягодный тарт — 1590₸</h4>
+                            <p class="m-0">
+                                Лёгкий десерт с песочной основой, кремом и свежими ягодами.
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
+    </div>  
 @endsection
